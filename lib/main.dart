d8a6acada019639bd5ce36aabf3e4bc0d47c09ef
KASRA10 <kasrasparrow@gmail.comm>
2024-10-08 23:33:41 +0330

    Add All List Tile And Contact Cards

--- lib/main.dart
+++ lib/main.dart
@@ -19,12 +19,12 @@ class BasicBusinessCardTwo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black54,
+      backgroundColor: Colors.black,
       appBar: AppBar(
         backgroundColor: Colors.white,
         centerTitle: true,
-        shadowColor: Colors.white24,
-        elevation: 15,
+        shadowColor: Colors.white,
+        elevation: 6,
         shape: const RoundedRectangleBorder(
           borderRadius: BorderRadius.only(
             bottomLeft: Radius.circular(
@@ -43,7 +43,7 @@ class BasicBusinessCardTwo extends StatelessWidget {
             onPressed: () => exit(0),
             icon: const Icon(
               FontAwesomeIcons.chevronLeft,
-              color: Colors.black54,
+              color: Colors.black,
               semanticLabel: 'Exit-Back Button',
               size: 24,
             ),
@@ -67,7 +67,7 @@ class BasicBusinessCardTwo extends StatelessWidget {
               right: 15.5,
             ),
             child: IconButton(
-              onPressed: () => showToastMessage(
+              onPressed: () => showMyToastMessage(
                 'Not Available',
               ),
               icon: const Icon(
@@ -82,19 +82,19 @@ class BasicBusinessCardTwo extends StatelessWidget {
           ),
         ], // End Of Actions[]
       ),
-      body: const SafeArea(
+      body: SafeArea(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            CircleAvatar(
+            const CircleAvatar(
               backgroundColor: Colors.white,
               backgroundImage: AssetImage(
                 'lib/images/KasraK10.png',
               ),
               radius: 70.0,
             ),
-            Text(
+            const Text(
               'KasraK10',
               style: TextStyle(
                 color: Colors.white,
@@ -104,31 +104,265 @@ class BasicBusinessCardTwo extends StatelessWidget {
                 fontWeight: FontWeight.bold,
               ),
             ),
-            Text(
-              'Flutter Developer',
-              style: TextStyle(
-                color: Colors.white,
-                fontFamily: 'Playwrite',
-                fontSize: 12,
-                fontStyle: FontStyle.italic,
-                fontWeight: FontWeight.bold,
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: 10.5,
+              ),
+              child: const Text(
+                'Flutter Developer',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontFamily: 'Playwrite',
+                  fontSize: 12,
+                  fontWeight: FontWeight.bold,
+                  fontStyle: FontStyle.italic,
+                ),
               ),
             ),
-            SizedBox(
+            const SizedBox(
               child: Divider(
                 color: Colors.white54,
                 endIndent: 150,
                 indent: 150,
               ),
             ),
-          ],
+            Container(
+              width: 300,
+              margin: const EdgeInsets.symmetric(
+                vertical: 5.0,
+              ),
+              child: Card(
+                color: Colors.white,
+                shadowColor: Colors.white54,
+                elevation: 4,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(
+                      50.0,
+                    ),
+                  ),
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    FontAwesomeIcons.github,
+                    color: Colors.black,
+                    semanticLabel: 'Github Official Logo',
+                  ),
+                  titleAlignment: ListTileTitleAlignment.center,
+                  title: Text(
+                    '@KASRA10',
+                    style: TextStyle(
+                      fontFamily: 'Playwrite',
+                      fontWeight: FontWeight.bold,
+                      fontStyle: FontStyle.italic,
+                      fontSize: 16,
+                    ),
+                  ),
+                  enableFeedback: true,
+                ),
+              ),
+            ),
+            Container(
+              width: 300,
+              margin: const EdgeInsets.symmetric(
+                vertical: 5.0,
+              ),
+              child: Card(
+                color: Colors.white,
+                shadowColor: Colors.white54,
+                elevation: 4,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(
+                      50.0,
+                    ),
+                  ),
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    FontAwesomeIcons.linkedinIn,
+                    color: Colors.black,
+                    semanticLabel: 'LinkedIn Official Logo',
+                  ),
+                  titleAlignment: ListTileTitleAlignment.center,
+                  title: Text(
+                    '@kasra-hosseini-k10',
+                    style: TextStyle(
+                      fontFamily: 'Playwrite',
+                      fontWeight: FontWeight.bold,
+                      fontStyle: FontStyle.italic,
+                      fontSize: 16,
+                    ),
+                  ),
+                  enableFeedback: true,
+                ),
+              ),
+            ),
+            Container(
+              width: 300,
+              margin: const EdgeInsets.symmetric(
+                vertical: 5.0,
+              ),
+              child: Card(
+                color: Colors.white,
+                shadowColor: Colors.white54,
+                elevation: 4,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(
+                      50.0,
+                    ),
+                  ),
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    FontAwesomeIcons.instagram,
+                    color: Colors.black,
+                    semanticLabel: 'Instagram Official Logo',
+                  ),
+                  titleAlignment: ListTileTitleAlignment.center,
+                  title: Text(
+                    '@kasrak10',
+                    style: TextStyle(
+                      fontFamily: 'Playwrite',
+                      fontWeight: FontWeight.bold,
+                      fontStyle: FontStyle.italic,
+                      fontSize: 16,
+                    ),
+                  ),
+                  enableFeedback: true,
+                ),
+              ),
+            ),
+            Container(
+              width: 300,
+              margin: const EdgeInsets.symmetric(
+                vertical: 5.0,
+              ),
+              child: Card(
+                color: Colors.white,
+                shadowColor: Colors.white54,
+                elevation: 4,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(
+                      50.0,
+                    ),
+                  ),
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    FontAwesomeIcons.xTwitter,
+                    color: Colors.black,
+                    semanticLabel: 'X (Twitter) Official Logo',
+                  ),
+                  titleAlignment: ListTileTitleAlignment.center,
+                  title: Text(
+                    '@H_Kasra10',
+                    style: TextStyle(
+                      fontFamily: 'Playwrite',
+                      fontWeight: FontWeight.bold,
+                      fontStyle: FontStyle.italic,
+                      fontSize: 16,
+                    ),
+                  ),
+                  enableFeedback: true,
+                ),
+              ),
+            ),
+            Container(
+              width: 300,
+              margin: const EdgeInsets.symmetric(
+                vertical: 5.0,
+              ),
+              child: Card(
+                color: Colors.white,
+                shadowColor: Colors.white54,
+                elevation: 4,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(
+                      50.0,
+                    ),
+                  ),
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    FontAwesomeIcons.facebookF,
+                    color: Colors.black,
+                    semanticLabel: 'Facebook Official Logo',
+                  ),
+                  titleAlignment: ListTileTitleAlignment.center,
+                  title: Text(
+                    '@Kasra10.Hosseini',
+                    style: TextStyle(
+                      fontFamily: 'Playwrite',
+                      fontWeight: FontWeight.bold,
+                      fontStyle: FontStyle.italic,
+                      fontSize: 16,
+                    ),
+                  ),
+                  enableFeedback: true,
+                ),
+              ),
+            ),
+            Container(
+              width: 300,
+              margin: const EdgeInsets.symmetric(
+                vertical: 5.0,
+              ),
+              child: Card(
+                color: Colors.white,
+                shadowColor: Colors.white54,
+                elevation: 4,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(
+                      50.0,
+                    ),
+                  ),
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.language_outlined,
+                    color: Colors.black,
+                    semanticLabel: 'Facebook Official Logo',
+                  ),
+                  titleAlignment: ListTileTitleAlignment.center,
+                  title: Text(
+                    '@kasra10design.com',
+                    style: TextStyle(
+                      fontFamily: 'Playwrite',
+                      fontWeight: FontWeight.bold,
+                      fontStyle: FontStyle.italic,
+                      fontSize: 16,
+                    ),
+                  ),
+                  enableFeedback: true,
+                ),
+              ),
+            ),
+          ], // End Of ChildrenWidgets Of Column
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => showMyToastMessage(
+          'Thanks For Support',
+        ),
+        mini: true,
+        backgroundColor: Colors.white,
+        elevation: 15,
+        tooltip: 'Tapping The Heart Icon As Like The App And Support',
+        child: Icon(
+          FontAwesomeIcons.heart,
+          semanticLabel: 'Heart Icon As Like The App And Support',
         ),
       ),
     );
   }
 
   // ToastMessage
-  void showToastMessage(String message) => Fluttertoast.showToast(
+  void showMyToastMessage(String message) => Fluttertoast.showToast(
         msg: message,
         backgroundColor: Colors.white,
         textColor: Colors.black54,
